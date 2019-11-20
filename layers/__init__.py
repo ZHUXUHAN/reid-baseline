9ffc81b3e7fb00bf91d1960a2332064b1a646520
ZXHUXUHAN <18811377800@163.com>
2019-11-20 19:49:09 +0800

    layers

---
+++ layers/__init__.py
@@ -0,0 +1,142 @@
+# encoding: utf-8
+"""
+@author:  liaoxingyu
+@contact: sherlockliao01@gmail.com
+"""
+
+import torch.nn.functional as F
+
+from .triplet_loss import TripletLoss, CrossEntropyLabelSmooth
+from .center_loss import CenterLoss
+from .local_loss import local_loss
+from .local_triplet import TripletLoss_Local
+
+import torch
+
+
+def make_loss(cfg, num_classes):  # modified by gu
+    sampler = cfg.DATALOADER.SAMPLER
+    if cfg.MODEL.METRIC_LOSS_TYPE == 'triplet':
+        triplet = TripletLoss(cfg.SOLVER.MARGIN)  # triplet loss
+    else:
+        print('expected METRIC_LOSS_TYPE should be triplet'
+              'but got {}'.format(cfg.MODEL.METRIC_LOSS_TYPE))
+
+    if cfg.MODEL.IF_LABELSMOOTH == 'on':
+        xent = CrossEntropyLabelSmooth(num_classes=num_classes)  # new add by luo
+        print("label smooth on, numclasses:", num_classes)
+
+    if sampler == 'softmax':
+        def loss_func(score, feat, target):
+            return F.cross_entropy(score, target)
+    elif cfg.DATALOADER.SAMPLER == 'triplet':
+        def loss_func(score, feat, target):
+            return triplet(feat, target)[0]
+    elif cfg.DATALOADER.SAMPLER == 'softmax_triplet':
+        def loss_func(score, feat, target, local_score, local_feat):
+            if cfg.MODEL.METRIC_LOSS_TYPE == 'triplet':
+                if cfg.MODEL.IF_LABELSMOOTH == 'on':
+                    global_loss = triplet(feat, target)[0]
+                    center_loss = cfg.SOLVER.CENTER_LOSS_WEIGHT * center_criterion(feat, target)
+                    global_score_loss = xent(score, target)
+                    tripletloss_local = TripletLoss_Local(0.3)
+                    if cfg.MODEL.ALIGNED:
+                        pinds, ginds = triplet(feat, target)[3], triplet(feat, target)[4]
+                        return xent(score, target) + \
+                               global_loss + center_loss + \
+                               local_loss(tripletloss_local, local_feat, pinds, ginds, target)[0]
+                    elif cfg.MODEL.PCB:
+                        return xent(score, target) + \
+                               global_loss + \
+                               sum(xent(s, target) for s in local_score) / len(local_score) + \
+                               sum(triplet(f, target)[0] for f in local_feat) / len(local_feat)
+
+                    else:
+                        return global_score_loss + global_loss + center_loss
+                else:
+                    if cfg.MODEL.ALIGNED:
+                        return F.cross_entropy(score, target) + triplet(feat, target)[0]
+                    else:
+                        return sum(F.cross_entropy(s, target) for s in score) / len(score) + sum(
+                            triplet(f, target)[0] for f in feat) / len(feat)
+
+            else:
+                print('expected METRIC_LOSS_TYPE should be triplet'
+                      'but got {}'.format(cfg.MODEL.METRIC_LOSS_TYPE))
+    else:
+        print('expected sampler should be softmax, triplet or softmax_triplet, '
+              'but got {}'.format(cfg.DATALOADER.SAMPLER))
+    return loss_func
+
+
+def make_loss_with_center(cfg, num_classes):  # modified by gu
+    if cfg.MODEL.NAME == 'resnet18' or cfg.MODEL.NAME == 'resnet34':
+        feat_dim = 512
+    elif cfg.MODEL.NAME == 'densenet161':
+        feat_dim = 2208
+    else:
+        feat_dim = 2048
+
+    if cfg.MODEL.METRIC_LOSS_TYPE == 'center':
+        center_criterion = CenterLoss(num_classes=num_classes, feat_dim=feat_dim, use_gpu=True)  # center loss
+
+    elif cfg.MODEL.METRIC_LOSS_TYPE == 'triplet_center':
+        triplet = TripletLoss(cfg.SOLVER.MARGIN)  # triplet loss
+        center_criterion = CenterLoss(num_classes=num_classes, feat_dim=feat_dim, use_gpu=True)  # center loss
+
+    else:
+        print('expected METRIC_LOSS_TYPE with center should be center, triplet_center'
+              'but got {}'.format(cfg.MODEL.METRIC_LOSS_TYPE))
+
+    if cfg.MODEL.IF_LABELSMOOTH == 'on':
+        xent = CrossEntropyLabelSmooth(num_classes=num_classes, use_focal=cfg.MODEL.USE_FOCAL_LOSS)  # new add by luo
+        print("label smooth on, numclasses:", num_classes)
+
+    def loss_func(score, feat, target, local_score, local_feat):  # local_feat
+        if cfg.MODEL.METRIC_LOSS_TYPE == 'center':
+            if cfg.MODEL.IF_LABELSMOOTH == 'on':
+                return xent(score, target) + \
+                       cfg.SOLVER.CENTER_LOSS_WEIGHT * center_criterion(feat, target)
+            else:
+                return F.cross_entropy(score, target) + \
+                       cfg.SOLVER.CENTER_LOSS_WEIGHT * center_criterion(feat, target)
+
+        elif cfg.MODEL.METRIC_LOSS_TYPE == 'triplet_center':
+            if cfg.MODEL.IF_LABELSMOOTH == 'on':
+                if cfg.MODEL.ALIGNED:
+                    tripletloss_local = TripletLoss_Local(0.3)
+                    pinds, ginds = triplet(feat, target)[3], triplet(feat, target)[4]
+                    global_loss = triplet(feat, target)[0]
+                    return xent(score, target) + \
+                           global_loss + \
+                           cfg.SOLVER.CENTER_LOSS_WEIGHT * center_criterion(feat, target) + \
+                           local_loss(tripletloss_local, local_feat, pinds, ginds, target)[0]
+                elif cfg.MODEL.PCB:
+                    global_loss = triplet(feat, target)[0]
+                    return xent(score, target) + \
+                           global_loss + \
+                           cfg.SOLVER.CENTER_LOSS_WEIGHT * center_criterion(feat, target) + \
+                           sum(xent(s, target) for s in local_score) / len(local_score)
+
+                elif cfg.MODEL.MGN:
+                    return sum(F.cross_entropy(s, target) for s in score) / len(score) + \
+                           sum(triplet(f, target)[0] for f in feat) / len(feat) + \
+                           cfg.SOLVER.CENTER_LOSS_WEIGHT * center_criterion(local_feat, target)
+                    # sum(xent(s, target) for s in score) / len(score) + \
+
+                else:
+                    global_loss = triplet(feat, target)[0]
+                    return xent(score, target) + \
+                           global_loss + \
+                           cfg.SOLVER.CENTER_LOSS_WEIGHT * center_criterion(feat, target)
+
+            else:
+                return F.cross_entropy(score, target) + \
+                       triplet(feat, target)[0] + \
+                       cfg.SOLVER.CENTER_LOSS_WEIGHT * center_criterion(feat, target)
+
+        else:
+            print('expected METRIC_LOSS_TYPE with center should be center, triplet_center'
+                  'but got {}'.format(cfg.MODEL.METRIC_LOSS_TYPE))
+
+    return loss_func, center_criterion
---
+++ layers/focal_loss.py
@@ -0,0 +1,81 @@
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# from torch.autograd import Variable
+
+
+# class FocalLoss(nn.Module):
+#     def __init__(self, gamma=2, alpha=0.25, size_average=True):
+#         super(FocalLoss, self).__init__()
+#         self.gamma = gamma
+#         self.alpha = alpha
+#         if isinstance(alpha, (float, int)): self.alpha = torch.Tensor([alpha, 1 - alpha])
+#         if isinstance(alpha, list): self.alpha = torch.Tensor(alpha)
+#         self.size_average = size_average
+#
+#     def forward(self, input, target):
+#         if input.dim() > 2:
+#             input = input.view(input.size(0), input.size(1), -1)  # N,C,H,W => N,C,H*W
+#             input = input.transpose(1, 2)  # N,C,H*W => N,H*W,C
+#             input = input.contiguous().view(-1, input.size(2))  # N,H*W,C => N*H*W,C
+#         target = target.view(-1, 1)
+#
+#         logpt = F.log_softmax(input)
+#         logpt = logpt.gather(1, target)
+#         logpt = logpt.view(-1)
+#         pt = Variable(logpt.data.exp())
+#
+#         if self.alpha is not None:
+#             if self.alpha.type() != input.data.type():
+#                 self.alpha = self.alpha.type_as(input.data)
+#             at = self.alpha.gather(0, target.data.view(-1))
+#             logpt = logpt * Variable(at)
+#
+#         loss = -1 * (1 - pt) ** self.gamma * logpt
+#         if self.size_average:
+#             return loss.mean()
+#         else:
+#             return loss.sum()
+
+
+####################################################
+##### This is focal loss class for multi class #####
+####################################################
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+# I refered https://github.com/c0nn3r/RetinaNet/blob/master/focal_loss.py
+
+class FocalLoss(nn.modules.loss._WeightedLoss):
+
+    def __init__(self, gamma=2, weight=None, size_average=None, ignore_index=-100,
+                 reduce=None, reduction='mean', balance_param=0.25):
+        super(FocalLoss, self).__init__(weight, size_average, reduce, reduction)
+        self.gamma = gamma
+        self.weight = weight
+        self.size_average = size_average
+        self.ignore_index = ignore_index
+        self.balance_param = balance_param
+
+    def forward(self, input, target):
+        # inputs and targets are assumed to be BatchxClasses
+        # print(input.shape)
+        # print(target.shape)
+        # assert len(input.shape) == len(target.shape)
+        # assert input.size(0) == target.size(0)
+        # assert input.size(1) == target.size(1)
+
+        weight = Variable(self.weight)
+
+        # compute the negative likelyhood
+        logpt = - F.binary_cross_entropy_with_logits(input, target, pos_weight=weight, reduction=self.reduction)
+        pt = torch.exp(logpt)
+
+        # compute the loss
+        focal_loss = -((1 - pt) ** self.gamma) * logpt
+        balanced_focal_loss = self.balance_param * focal_loss
+        return balanced_focal_loss
