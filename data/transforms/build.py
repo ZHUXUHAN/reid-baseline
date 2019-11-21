96e7979e1450aa6634965101ccd7a5b0e47d2ebf
ZHUXUHAN <18811377800@163.com>
2019-11-21 21:01:16 +0800

    Update build.py

--- data/transforms/build.py
+++ data/transforms/build.py
@@ -6,7 +6,42 @@
 
 import torchvision.transforms as T
 
-from .transforms import RandomErasing
+from .transforms import RandomErasing# encoding: utf-8
+"""
+@author:  liaoxingyu
+@contact: liaoxingyu2@jd.com
+"""
+
+import torchvision.transforms as T
+
+from .transforms import RandomErasing, RandomResize
+
+
+def build_transforms(cfg, is_train=True):
+    normalize_transform = T.Normalize(mean=cfg.INPUT.PIXEL_MEAN, std=cfg.INPUT.PIXEL_STD)
+    PIXEL_MEAN = (0.212737, 0.1830550, 0.097212)
+
+    if is_train:
+        transform = T.Compose([
+            T.Resize(cfg.INPUT.SIZE_TRAIN),
+            T.RandomHorizontalFlip(p=cfg.INPUT.PROB),
+            RandomResize(384, 128),
+            T.Pad(cfg.INPUT.PADDING),
+            T.RandomCrop(cfg.INPUT.SIZE_TRAIN),
+            T.ToTensor(),
+            normalize_transform,
+            RandomErasing(probability=cfg.INPUT.RE_PROB, mean=PIXEL_MEAN),
+
+        ])
+    else:
+        transform = T.Compose([
+            T.Resize(cfg.INPUT.SIZE_TEST),
+            T.ToTensor(),
+            normalize_transform
+        ])
+
+    return transform
+
 
 
 def build_transforms(cfg, is_train=True):
