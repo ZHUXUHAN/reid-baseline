4cd5ddc2f75afa5c9246803a852d065e3819bd7f
ZHUXUHAN <18811377800@163.com>
2019-11-25 23:17:03 +0800

    Update train_r50_ibn_mgn.sh

--- scripts/train_r50_ibn_mgn.sh
+++ scripts/train_r50_ibn_mgn.sh
@@ -1,6 +1,7 @@
 #SOLVER.BASE_LR 0.01
-python tools/train.py --config_file='configs/softmax_triplet_with_center.yml' \
-MODEL.DEVICE_ID "('2, 6')" \
+#MODEL.PRETRAIN_CHOICE 'self' \
+python tools/train.py --config_file='configs/softmax_triplet.yml' \
+MODEL.DEVICE_ID "('0,1')" \
 MODEL.NAME "('resnet50_ibn_a')" \
 DATASETS.NAMES "('market1501')" \
 DATASETS.ROOT_DIR "('/home/zxh/datasets')" \
@@ -9,5 +10,8 @@ OUTPUT_DIR "('new_experiment/r50_ibn_mgn')" \
 MODEL.MGN True \
 INPUT.SIZE_TRAIN "([384, 128])" \
 MODEL.LAST_STRIDE 2 \
-INPUT.RE_PROB 0.0 \
-SOLVER.MARGIN 1.3
+INPUT.RE_PROB 0.3 \
+SOLVER.MARGIN 1.2 \
+MODEL.IF_LABELSMOOTH 'off' \
+SOLVER.CHECKPOINT_PERIOD 20 \
+SOLVER.MAX_EPOCHS 180
