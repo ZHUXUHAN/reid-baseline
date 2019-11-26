0aca0282287a1286dd400e3d6a2341e533442551
ZHUXUHAN <18811377800@163.com>
2019-11-26 15:40:56 +0800

    Update focal_loss.py

--- layers/focal_loss.py
+++ layers/focal_loss.py
@@ -47,35 +47,102 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-
 # I refered https://github.com/c0nn3r/RetinaNet/blob/master/focal_loss.py
+#
+# class FocalLoss(nn.modules.loss._WeightedLoss):
+#
+#     def __init__(self, gamma=2, weight=None, size_average=None, ignore_index=-100,
+#                  reduce=None, reduction='mean', balance_param=0.25):
+#         super(FocalLoss, self).__init__(weight, size_average, reduce, reduction)
+#         self.gamma = gamma
+#         self.weight = weight
+#         self.size_average = size_average
+#         self.ignore_index = ignore_index
+#         self.balance_param = balance_param
+#
+#     def forward(self, input, target):
+#         # inputs and targets are assumed to be BatchxClasses
+#         # print(input.shape)
+#         # print(target.shape)
+#         # assert len(input.shape) == len(target.shape)
+#         # assert input.size(0) == target.size(0)
+#         # assert input.size(1) == target.size(1)
+#
+#         weight = Variable(self.weight)
+#
+#         # compute the negative likelyhood
+#         logpt = - F.binary_cross_entropy_with_logits(input, target, pos_weight=weight, reduction=self.reduction)
+#         pt = torch.exp(logpt)
+#
+#         # compute the loss
+#         focal_loss = -((1 - pt) ** self.gamma) * logpt
+#         balanced_focal_loss = self.balance_param * focal_loss
+#         return balanced_focal_loss
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
 
-class FocalLoss(nn.modules.loss._WeightedLoss):
 
-    def __init__(self, gamma=2, weight=None, size_average=None, ignore_index=-100,
-                 reduce=None, reduction='mean', balance_param=0.25):
-        super(FocalLoss, self).__init__(weight, size_average, reduce, reduction)
+class FocalLoss(nn.Module):
+    r"""
+        This criterion is a implemenation of Focal Loss, which is proposed in
+        Focal Loss for Dense Object Detection.
+
+            Loss(x, class) = - \alpha (1-softmax(x)[class])^gamma \log(softmax(x)[class])
+
+        The losses are averaged across observations for each minibatch.
+
+        Args:
+            alpha(1D Tensor, Variable) : the scalar factor for this criterion
+            gamma(float, double) : gamma > 0; reduces the relative loss for well-classiﬁed examples (p > .5),
+                                   putting more focus on hard, misclassiﬁed examples
+            size_average(bool): By default, the losses are averaged over observations for each minibatch.
+                                However, if the field size_average is set to False, the losses are
+                                instead summed for each minibatch.
+
+
+    """
+
+    def __init__(self, class_num, alpha=None, gamma=2, size_average=True):
+        super(FocalLoss, self).__init__()
+        if alpha is None:
+            self.alpha = Variable(torch.ones(class_num, 1))
+        else:
+            if isinstance(alpha, Variable):
+                self.alpha = alpha
+            else:
+                self.alpha = Variable(alpha)
         self.gamma = gamma
-        self.weight = weight
+        self.class_num = class_num
         self.size_average = size_average
-        self.ignore_index = ignore_index
-        self.balance_param = balance_param
 
-    def forward(self, input, target):
-        # inputs and targets are assumed to be BatchxClasses
-        # print(input.shape)
-        # print(target.shape)
-        # assert len(input.shape) == len(target.shape)
-        # assert input.size(0) == target.size(0)
-        # assert input.size(1) == target.size(1)
+    def forward(self, inputs, targets):
+        N = inputs.size(0)
+        C = inputs.size(1)
+        P = F.softmax(inputs, dim=1)
+        class_mask = inputs.data.new(N, C).fill_(0)
+        class_mask = Variable(class_mask)
+        ids = targets.view(-1, 1)
+        class_mask.scatter_(1, ids.data, 1.)
+        # print(class_mask)
 
-        weight = Variable(self.weight)
+        if inputs.is_cuda and not self.alpha.is_cuda:
+            self.alpha = self.alpha.cuda()
+        alpha = self.alpha[ids.data.view(-1)]
 
-        # compute the negative likelyhood
-        logpt = - F.binary_cross_entropy_with_logits(input, target, pos_weight=weight, reduction=self.reduction)
-        pt = torch.exp(logpt)
+        probs = (P * class_mask).sum(1).view(-1, 1)
 
-        # compute the loss
-        focal_loss = -((1 - pt) ** self.gamma) * logpt
-        balanced_focal_loss = self.balance_param * focal_loss
-        return balanced_focal_loss
+        log_p = probs.log()
+        # print('probs size= {}'.format(probs.size()))
+        # print(probs)
+
+        batch_loss = -alpha * (torch.pow((1 - probs), self.gamma)) * log_p
+        # print('-----bacth_loss------')
+        # print(batch_loss)
+
+        if self.size_average:
+            loss = batch_loss.mean()
+        else:
+            loss = batch_loss.sum()
+        return loss
