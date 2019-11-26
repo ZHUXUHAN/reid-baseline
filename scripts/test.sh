9e5b22e5044caf035b653e888dcef09a7c1170ab
ZHUXUHAN <18811377800@163.com>
2019-11-26 19:45:21 +0800

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,21 +2,25 @@
 #INPUT.PIXEL_MEAN "([0.097, 0.1831, 0.2127])" \
 #INPUT.PIXEL_STD "([0.1356, 0.1482, 0.1938])"
 # 注意每次生成新的json文件时 要把data 数据流路径改了
-# 测试流程：1、先调rerank ADJUST_RERANK:True MERGE:False 2、再保存npy，把数据集改掉  ADJUST_RERANK:False MERGE:False 3、cat生成json  ADJUST_RERANK:False MERGE:True
+# 测试流程：1、先调rerank ADJUST_RERANK:True MERGE:False 2、再保存npy，把数据集改掉,把底下的数据增强代码改掉  ADJUST_RERANK:False MERGE:False 3、cat生成json  ADJUST_RERANK:False MERGE:True
 # 注意过程中时刻要调整路径
 python tools/test.py \
---config_file='configs/softmax_triplet_with_center.yml' \
-MODEL.DEVICE_ID "('6,7')" \
+--config_file='configs/softmax_triplet.yml' \
+MODEL.DEVICE_ID "('0,1')" \
 MODEL.NAME "('resnet50_ibn_a')" \
 DATASETS.NAMES "('market1501')" \
 DATASETS.ROOT_DIR "('/home/zxh/datasets')" \
 MODEL.PRETRAIN_CHOICE "('self')" \
-TEST.WEIGHT "('/home/zxh/reid-strong-baseline/new_experiment/r50_ibn_pcb_sampler/resnet50_ibn_a_model_120.pth')" \
+TEST.WEIGHT "('/home/zxh/reid-baseline/new_experiment/r50_ibn_pcb_diedai/resnet50_ibn_a_model_80.pth')" \
 TEST.RE_RANKING  "('yes')" \
 MODEL.ALIGNED False \
 MODEL.PCB True \
+MODEL.NEW_PCB False \
+MODEL.NEW_PCB False \
+MODEL.MGN False \
 INPUT.SIZE_TEST "([384, 128])" \
 TEST.ADJUST_RERANK False \
 MODEL.CAM False \
-TEST.MERGE False
-
+TEST.MERGE True \
+OUTPUT_DIR "" \
+MODEL.LAST_STRIDE 1
