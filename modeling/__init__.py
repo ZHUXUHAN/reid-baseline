43b2a33d4b14c4ad6d5842308bb59e8b2465886e
ZHUXUHAN <18811377800@163.com>
2019-12-03 09:49:02 +0800

    Update __init__.py

--- modeling/__init__.py
+++ modeling/__init__.py
@@ -8,6 +8,7 @@ from .baseline import Baseline
 from .aligned_baseline import AlignedBaseline
 from .pcb_baseline import PCBBaseline
 from .mgn_baseline import MGNBaseline
+from .new_pcb_baseline import NEW_PCBBaseline
 
 
 def build_model(cfg, num_classes):
@@ -22,6 +23,11 @@ def build_model(cfg, num_classes):
     elif cfg.MODEL.MGN:
         model = MGNBaseline(num_classes, cfg.MODEL.LAST_STRIDE, cfg.MODEL.PRETRAIN_PATH, cfg.MODEL.NECK,
                             cfg.TEST.NECK_FEAT, cfg.MODEL.NAME, cfg.MODEL.PRETRAIN_CHOICE, cfg.MODEL.WITH_GCB)
+
+    elif cfg.MODEL.NEW_PCB:
+        model = NEW_PCBBaseline(num_classes, cfg.MODEL.LAST_STRIDE, cfg.MODEL.PRETRAIN_PATH, cfg.MODEL.NECK,
+                                cfg.TEST.NECK_FEAT, cfg.MODEL.NAME, cfg.MODEL.PRETRAIN_CHOICE, cfg.MODEL.PCB_NUM_STRIPES,
+                            cfg.MODEL.WITH_GCB, cfg.MODEL.PCB_RPP, cfg.MODEL.DROPOUT, cfg.MODEL.CAM, cfg.MODEL.SUM, cfg.MODEL.ARC)
 
     else:
         model = Baseline(num_classes, cfg.MODEL.LAST_STRIDE, cfg.MODEL.PRETRAIN_PATH, cfg.MODEL.NECK,
