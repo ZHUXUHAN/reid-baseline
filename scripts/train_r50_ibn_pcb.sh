1ceb678ae8d8d6d3336706c0cb96c9f1fb54ed93
ZHUXUHAN <18811377800@163.com>
2019-12-04 19:45:40 +0800

    Update train_r50_ibn_pcb.sh

--- scripts/train_r50_ibn_pcb.sh
+++ scripts/train_r50_ibn_pcb.sh
@@ -9,7 +9,7 @@ MODEL.NAME "('resnet50_ibn_a')" \
 DATASETS.NAMES "('market1501')" \
 DATASETS.ROOT_DIR "('/home/zxh/datasets')" \
 MODEL.PRETRAIN_PATH "('/home/zxh/.cache/torch/checkpoints/r50_ibn_a.pth')" \
-OUTPUT_DIR "('new_experiment/r50_ibn_pcb_sampler')" \
+OUTPUT_DIR "('new_experiment/r50_ibn_pcb_newlr')" \
 MODEL.PCB True \
 INPUT.SIZE_TRAIN "([384, 128])" \
 INPUT.RE_PROB 0.3 \
@@ -17,6 +17,9 @@ MODEL.PCB_RPP False \
 MODEL.USE_FOCAL_LOSS False \
 MODEL.SUM False \
 SOLVER.MAX_EPOCHS 180 \
-SOLVER.CHECKPOINT_PERIOD 20
-
-
+SOLVER.CHECKPOINT_PERIOD 20 \
+SOLVER.STEPS "([30, 60, 90])" \
+SOLVER.BASE_LR 0.0035 \
+SOLVER.WARMUP_ITERS 100 \
+SOLVER.EVAL_PERIOD 10 \
+INPUT.SIZE_TEST "([384, 128])"
