b5e0ca0660e9a68760d1e4b47b9195fa90bf06a4
ZHUXUHAN <18811377800@163.com>
2019-12-08 18:31:45 +0800

    Create stn.py

---
+++ modeling/stn.py
@@ -0,0 +1,45 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+
+class STN(nn.Module):
+    def __init__(self):
+        super(STN, self).__init__()
+
+        self.localization = nn.Sequential(
+            nn.Conv2d(3, 8, kernel_size=7),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d(8, 10, kernel_size=5),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True)
+        )
+
+        # Regressor for the 3 * 2 affine matrix
+        self.fc_loc = nn.Sequential(
+            nn.Linear(10 * 92 * 28, 32),
+            nn.ReLU(True),
+            nn.Linear(32, 3 * 2)
+        )
+        # Initialize the weights/bias with identity transformation
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        xs = self.localization(x)
+        xs = xs.view(-1, 10 * 92 * 28)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+
+        return x
