bf1eb07e6d40c2cb2ee8df8f93ef64d6bd588f4c
ZHUXUHAN <18811377800@163.com>
2019-12-08 13:39:15 +0800

    Create classblock.py

---
+++ modeling/classblock.py
@@ -0,0 +1,56 @@
+import torch.nn as nn
+from torch.nn import init
+
+def weights_init_kaiming(m):
+    classname = m.__class__.__name__
+    # print(classname)
+    if classname.find('Conv2d') != -1:
+        init.kaiming_normal(m.weight.data, a=0, mode='fan_in')
+        init.constant(m.bias.data, 0.0)
+    elif classname.find('Linear') != -1:
+        init.kaiming_normal(m.weight.data, a=0, mode='fan_out')
+        #init.constant(m.bias.data, 0.0)
+    elif classname.find('BatchNorm1d') != -1:
+        init.normal(m.weight.data, 1.0, 0.02)
+        init.constant(m.bias.data, 0.0)
+
+
+def weights_init_classifier(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        init.normal(m.weight.data, std=0.001)
+        #init.constant(m.bias.data, 0.0)
+
+
+class ClassBlock(nn.Module):
+    def __init__(self, input_dim, class_num, relu=True, num_bottleneck=512):
+        super(ClassBlock, self).__init__()
+        # add_block = []
+        add_block1 = []
+        add_block2 = []
+        add_block1 += [nn.BatchNorm1d(input_dim)]
+        if relu:
+            add_block1 += [nn.LeakyReLU(0.1)]
+        add_block1 += [nn.Linear(input_dim, num_bottleneck, bias=False)]
+        add_block2 += [nn.BatchNorm1d(num_bottleneck)]
+
+        # add_block = nn.Sequential(*add_block)
+        # add_block.apply(weights_init_kaiming)
+        add_block1 = nn.Sequential(*add_block1)
+        add_block1.apply(weights_init_kaiming)
+        add_block2 = nn.Sequential(*add_block2)
+        add_block2.apply(weights_init_kaiming)
+        classifier = []
+        classifier += [nn.Linear(num_bottleneck, class_num, bias=False)]
+        classifier = nn.Sequential(*classifier)
+        classifier.apply(weights_init_classifier)
+
+        self.add_block1 = add_block1
+        self.add_block2 = add_block2
+        self.classifier = classifier
+
+    def forward(self, x):
+        x = self.add_block1(x)
+        x1 = self.add_block2(x)
+        x2 = self.classifier(x1)
+        return x, x1, x2
