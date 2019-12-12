9d1c64ea1b86330b71d5ac3dc7b0c3003655441d
ZHUXUHAN <18811377800@163.com>
2019-12-12 11:10:09 +0800

    Update pre_label.py

--- pre_data/pre_label.py
+++ pre_data/pre_label.py
@@ -1,16 +1,31 @@
 import shutil
 import os
 
-img_root = '/home/zxh/database/2019-NAIC-ReID/初赛A榜测试集/query_a'
-copy_to_root = '/home/zxh/ReID/deep-person-reid/data/NAICReID/query'
 
-with open("/home/zxh/database/2019-NAIC-ReID/初赛A榜测试集/query_a_list.txt") as f:
-    lines = f.readlines()
-max = 0
-for line in lines:
-    line = line.strip().split(' ')
-    imgname = line[0].split('/')[1]
-    imgid = line[1]
-    newimgname = '0000_c1s1_{}_00.png'.format(imgname.split('.')[0])
-    print(newimgname)
-    shutil.copy(os.path.join(img_root, imgname), os.path.join(copy_to_root, newimgname))
+def pre_train_label():
+    img_root = '/home/zxh/datasets/bounding_box_train'
+    copy_to_root = '/home/zxh/datasets/copy_train'
+
+    with open("/home/zxh/train_list.txt") as f:
+        lines = f.readlines()
+    max = 0
+    for line in lines:
+        line = line.strip().split(' ')
+        imgname = line[0].split('/')[1]
+        imgid = line[1]
+        newimgname = '{}_c1s1_{}_00.png'.format(imgid, imgname.split('.')[0])
+        print(newimgname)
+
+        shutil.copy(os.path.join(img_root, imgname), os.path.join(copy_to_root, newimgname))
+
+
+def pre_test_label():
+    img_root = '/home/zxh/datasets/bounding_box_test'
+    copy_to_root = '/home/zxh/datasets/copy_test'
+    files = os.listdir(img_root)
+    for file in files:
+        newimgname = '{}_c1s1_{}_00.png'.format('0000', file.split('.')[0])
+        print(newimgname)
+        shutil.copy(os.path.join(img_root, file), os.path.join(copy_to_root, newimgname))
+
+pre_test_label()
