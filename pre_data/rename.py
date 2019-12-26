d59b0f3d57454e1c21c9efc128b4ef2b05528de7
ZHUXUHAN <18811377800@163.com>
2019-12-26 17:24:56 +0800

    Update rename.py

--- pre_data/rename.py
+++ pre_data/rename.py
@@ -12,7 +12,8 @@ gallery_files = os.listdir(gallery_path)
 for file in query_files:
     query_id = file.split('_')[0]
     query_newid = int(query_id)+9968
-    query_newname = file.replace(file[:4], str(query_newid))
+    file_front = file.split('_')[0]
+    query_newname = file.replace(file_front, str(query_newid))
     print(query_newname)
     ori_path = os.path.join(query_path, file)
     new_path = os.path.join(query_newpath, query_newname)
@@ -23,8 +24,9 @@ for file in query_files:
 for file in gallery_files:
     gallery_id = file.split('_')[0]
     gallery_newid = int(gallery_id)+9968
-    gallery_newname = file.replace(file[:4], str(gallery_newid))
-    print(query_newname)
+    file_front = file.split('_')[0]
+    gallery_newname = file.replace(file_front, str(gallery_newid))
+    print(gallery_newname)
     ori_path = os.path.join(gallery_path, file)
     new_path = os.path.join(gallery_newpath, gallery_newname)
 
